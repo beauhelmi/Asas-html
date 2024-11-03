79e0baed1622639df27e4c08746599e569b1cad3
beauhelmi <beauhelmi@gmail.com>
2024-11-03 20:54:25 +0800

    Demonstrate basic arithmetic operations with addition, subtraction, multiplication, division, and modulus in PHP

--- operator-1.php
+++ operator-1.php
@@ -1,12 +1,10 @@
 <?php 
+/*
     $x = 1;
 
     echo "$x <br>";
-
     $x = 15 - 4; // tolak, hasil 11
-
     echo "$x <br>";
-
     $x = 15 + 4; // tambah, hasil 19
     echo "$x <br>";
     $x = 15 / 4; // bahagi, hasil 3.75
@@ -16,4 +14,24 @@
     $x = 15 % 4; // modulus (baki), hasil 3
     echo "$x <br>";
     $x = 15 ** 4; // kuasa, hasil 50,625
-?>
+    echo "$x <br>";
+*/
+
+    $x = 15; // memberi nilai 15
+    echo "$x <br>";
+    
+    $x += 15; // ditambah 15
+    echo "$x <br>";
+    
+    $x -= 15; // ditolak 15
+    echo "$x <br>";
+
+    $x *= 15; // didarab 15
+    echo "$x <br>";
+
+    $x /= 15; // dibahagi 15
+    echo "$x <br>";
+
+    $x %= 15; // operasi modulus
+    echo "$x <br>";
+    ?>
