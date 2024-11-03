2f6fe3c44fbcf01aa32e7f5a6199d7390b2eca0d
beauhelmi <beauhelmi@gmail.com>
2024-11-03 20:46:17 +0800

    add echo on each line

--- operator-1.php
+++ operator-1.php
@@ -8,8 +8,12 @@
     echo "$x <br>";
 
     $x = 15 + 4; // tambah, hasil 19
+    echo "$x <br>";
     $x = 15 / 4; // bahagi, hasil 3.75
+    echo "$x <br>";
     $x = 15 * 4; // darab, hasil 60
+    echo "$x <br>";
     $x = 15 % 4; // modulus (baki), hasil 3
+    echo "$x <br>";
     $x = 15 ** 4; // kuasa, hasil 50,625
 ?>
