1e5b882e9f3cc42091474fedf53ed6b7d8c07b29
beauhelmi <beauhelmi@gmail.com>
2024-11-03 20:44:16 +0800

    first exercise

---
+++ operator-1.php
@@ -0,0 +1,15 @@
+<?php 
+    $x = 1;
+
+    echo "$x <br>";
+
+    $x = 15 - 4; // tolak, hasil 11
+
+    echo "$x <br>";
+
+    $x = 15 + 4; // tambah, hasil 19
+    $x = 15 / 4; // bahagi, hasil 3.75
+    $x = 15 * 4; // darab, hasil 60
+    $x = 15 % 4; // modulus (baki), hasil 3
+    $x = 15 ** 4; // kuasa, hasil 50,625
+?>
